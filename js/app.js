5231b736581847a8f89b7bd9a90655cabddc6e9a
Ibrahim El-Mokhtar <ibrahimelmokhtar@gmail.com>
2022-03-14 15:54:29 +0200

    Add copy to clipboard functionality

--- js/app.js
+++ js/app.js
@@ -5,14 +5,40 @@ const hexCharacterArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e'
 // generated palette:
 let generatedPalette = [];
 
+// obtain required DOM elements:
+const displayedListObject = document.querySelector('#generated__list');
+const copiedMessageObject = document.querySelector('#copied');
+
+
+/**
+ * @description Copy specific color code to clipboard.
+ * @param {Event} event
+ */
+const copyToClipboard = (event) => {
+    // check the clicked icon:
+    if ((event.target.nodeName === 'I') && (event.target.classList.contains('fa-clone'))) {
+        // obtain the color code text:
+        const text = event.target.parentElement.querySelector('span').textContent;
+
+        // copy text to clipboard:
+        navigator.clipboard.writeText(text);
+
+        // modify the displayed message style:
+        copiedMessageObject.style.color = text;
+        copiedMessageObject.style.bottom = '50%';
+
+        // fade out the displayed message after specific time interval:
+        setTimeout(() => {
+            copiedMessageObject.style.bottom = '-100%';
+        }, 2000);
+    }
+};
+
 
 /**
  * @description Display the generated palette on the screen.
  */
 const displayPalette = () => {
-    // obtain required DOM elements:
-    const displayedListObject = document.querySelector('#generated__list');
-
     // create virtual DOM element:
     const fragment = document.createDocumentFragment();
 
@@ -20,12 +46,13 @@ const displayPalette = () => {
     for (let i=0; i<generatedPalette.length; i++) {
         // create required elements:
         const singleColorObject = document.createElement('li');
-        const singleColorTextObject = document.createElement('span');
-        singleColorObject.appendChild(singleColorTextObject);
+        singleColorObject.innerHTML = `
+            <i class="far fa-clone" title="Copy to clipboard"></i>
+            <span></span>`;
 
         // set the values to the current color value:
-        singleColorTextObject.textContent = generatedPalette[i];
-        singleColorTextObject.style.color = generatedPalette[i];
+        singleColorObject.querySelector('span').textContent = generatedPalette[i];
+        singleColorObject.style.color = generatedPalette[i];
         singleColorObject.style.backgroundColor = generatedPalette[i];
 
         // append the color to the virtual object:
@@ -61,8 +88,9 @@ const generateSingleColor = () => {
 const generateColorPalette = (event) => {
     // spacebar is pressed:
     if (event.keyCode === 32 || event === 'null') {
-        // clear the generated palette:
+        // reset the environment:
         generatedPalette = [];
+        copiedMessageObject.style.bottom = '-100%';
 
         // generate SIX random colors:
         for (let i=0; i<6; i++) {
@@ -82,4 +110,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // spacebar event listener:
     document.addEventListener('keyup', generateColorPalette);
+
+    // copy to clipboard:
+    displayedListObject.addEventListener('click', copyToClipboard);
 });
